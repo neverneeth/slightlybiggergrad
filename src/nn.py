8e2f0738e7c64a8412e2297812e87f8fedc57009
neverneeth <navaneethvsankar@gmail.com>
2026-06-09 15:31:11 +0530

    Basic Layer

---
+++ src/nn.py
@@ -0,0 +1,14 @@
+from engine import Tensor
+import numpy as np
+
+class Linear:
+    def __init__(self, nin, nout):
+        self.W = Tensor(np.random.randn(nin, nout), label='W')
+        self.b = Tensor(np.random.randn(nout), label='b')
+    
+    def __call__(self, x):
+        x = x if isinstance(x, Tensor) else Tensor(x)
+        return x @ self.W + self.b
+    
+    def parameters(self):
+        return (self.W, self.b)

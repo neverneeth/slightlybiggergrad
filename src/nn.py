b9abfeee831fe73cbee54c0d9ebd711979061296
neverneeth <navaneethvsankar@gmail.com>
2026-06-09 15:45:03 +0530

    MLP - forward pass

--- src/nn.py
+++ src/nn.py
@@ -16,3 +16,32 @@ class Linear:
     def zero_grad(self):
         for p in self.parameters():
             p.grad = np.zeros_like(p.data)
+
+class MLP:
+    def __init__(self, nin, nout, activation=None):
+        self.layers = []
+        self.activation = activation
+        for ns in nout:
+            layer = Linear(nin, ns)
+            self.layers.append(layer)
+            nin = ns
+            
+            
+
+    def __call__(self, x):
+        x = x if isinstance(x, Tensor) else Tensor(x)
+        for i, layer in enumerate(self.layers):
+            x = layer(x)
+            if self.activation and i < len(self.layers) - 1:
+                x = self.activation(x)
+        return x
+    
+    def parameters(self):
+        params = []
+        for layer in self.layers:
+            params.extend(layer.parameters())
+        return params
+    
+    def zero_grad(self):
+        for p in self.parameters():
+            p.grad = np.zeros_like(p.data)
